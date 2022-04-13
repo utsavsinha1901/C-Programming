a557af787b75b15897b254febcbb9483757dae09
Utsav <utsavsinha1901@gmail.com>
2022-04-14 00:49:59 +0530

    Modified

--- CwithGit.c
+++ CwithGit.c
@@ -1,5 +1,13 @@
+#include <stdio.h>
 int main()
 {
     /* code */
+    //This program is used to just learn Git.
+    int a,b,s;
+    printf("Enter the two numbers:\n");
+    scanf("%d %d",&a,&b);
+    s=a+b;
+    printf("The Sum is %d",s);
+
     return 0;
 }
