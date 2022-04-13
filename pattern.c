54f834149d471bc0b4cb6647beb698cf4b569e5d
utsavsinha1901 <102923242+utsavsinha1901@users.noreply.github.com>
2022-04-14 00:32:35 +0530

    Update pattern.c

--- pattern.c
+++ pattern.c
@@ -6,7 +6,7 @@ int main()
     if(opt==1){
         for(int i=1;i<=5;i++){
             for(int j=1;j<=i;j++){
-                printf("%c",*);
+                printf("%d",j);
             }
             printf();
         
